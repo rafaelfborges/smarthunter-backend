6fb49dc0ca0b66dcbb1ad086828ca27f49a27bdd
roos <rafael.borges@gft.com>
2021-03-09 14:49:50 -0300

    Setup base project with Spring 2.3.9

---
+++ src/main/java/com/openedu/mscourses/MsCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.openedu.mscourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openedu/mscourses/MsCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openedu.mscourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
