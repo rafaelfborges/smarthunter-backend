47189a8c98521a41849a2ae05061ff50ffb20a42
Rafael Borges <rafaelfborges@gmail.com>
2021-04-12 19:47:43 -0300

    Add findById in student service

--- src/main/java/com/smarthunter/api/services/impl/CourseService.java
+++ src/main/java/com/smarthunter/api/services/impl/CourseService.java
@@ -4,7 +4,6 @@ package com.smarthunter.api.services.impl;
 import com.smarthunter.api.entities.Course;
 import com.smarthunter.api.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,9 +13,6 @@ public class CourseService {
 
     @Autowired
     private CourseRepository repository;
-
-    @Autowired
-    private ApplicationEventPublisher applicationEventPublisher;
 
     public List<Course> findAll() {
         return repository.findAll();
--- src/main/java/com/smarthunter/api/services/impl/StudentService.java
+++ src/main/java/com/smarthunter/api/services/impl/StudentService.java
@@ -1,12 +1,14 @@
 package com.smarthunter.api.services.impl;
 
 
+import com.smarthunter.api.entities.EnrolledCourses;
 import com.smarthunter.api.entities.Student;
 import com.smarthunter.api.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class StudentService {
@@ -18,7 +20,12 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    public Student findById(Long id) {
+        return studentRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
     public Student save(Student student) {
         return studentRepository.save(student);
     }
+
 }
