475cad2c5281474639ce1db5d2681491d92aa5a6
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2021-04-13 21:24:20 -0300

    fazendo a configuração do spring fox que é responsavel por gerar o swagger da apicação

--- src/main/java/com/smarthunter/api/resources/CourseResource.java
+++ src/main/java/com/smarthunter/api/resources/CourseResource.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/courses")
+@RequestMapping("/courses")
 public class CourseResource {
 
     @Autowired
