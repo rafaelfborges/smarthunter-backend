8a6c3684c10edaa4ceb9a4a1df155474f66bfed2
Rafael Borges <rafaelfborges@gmail.com>
2021-05-17 19:59:51 -0300

    Add exception handle to username not found

--- src/main/java/com/smarthunter/api/handlers/GeneralExceptionHandler.java
+++ src/main/java/com/smarthunter/api/handlers/GeneralExceptionHandler.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -91,6 +92,15 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        String userMessage = ex.getMessage();
+        String devMessage = ExceptionUtils.getRootCauseMessage(ex);
+
+        List<Error> errors = Collections.singletonList(new Error(userMessage, devMessage));
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler({UsernameNotFoundException.class})
+    public ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException ex, WebRequest request) {
         String userMessage = ex.getMessage();
         String devMessage = ExceptionUtils.getRootCauseMessage(ex);
 
