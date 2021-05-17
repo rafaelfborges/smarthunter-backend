50bd3d113889c661d8a3f247c81f87c2e410466c
Rafael Borges <rafaelfborges@gmail.com>
2021-05-17 19:26:08 -0300

    Adjust some configurations for Spring Fox

--- src/main/java/com/smarthunter/api/configs/SwaggerConfig.java
+++ src/main/java/com/smarthunter/api/configs/SwaggerConfig.java
@@ -1,12 +1,12 @@
-package com.smarthunter.api.config;
+package com.smarthunter.api.configs;
 
 import com.google.common.collect.Lists;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
@@ -16,25 +16,23 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.ApiKeyVehicle;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
-    @Value("${project.version}")
-    private String version;
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                //para mudar o escaneamento do swagger, remover/mudar o base package abaixo
-                .apis(RequestHandlerSelectors.basePackage("com.smarthunter.api.resources"))
+                .apis(RequestHandlerSelectors.basePackage("com.smarthunter.api"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo())
+                .tags(new Tag("Courses", "Management Courses"))
+                .tags(new Tag("Users", "Management Users"))
+                .tags(new Tag("Enrolled Courses", "Management User Enrolled Courses"))
                 .securitySchemes(Lists.newArrayList(apiKey()))
                 .securityContexts(Lists.newArrayList(securityContext()));
     }
@@ -59,9 +57,12 @@ public class SwaggerConfig implements WebMvcConfigurer {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo("Back-end SmartHunter",
-                "Api back-end", version, "", new Contact("SmartHunter", "www.smarthunter.com", "contact@smarthunter.com"),
-                "License of API", "API license URL", Collections.emptyList());
+        return new ApiInfoBuilder()
+                .title("Back-end SmartHunter")
+                .description("API for Course Platform")
+                .version("1")
+                .contact(new Contact("SmartHunter", "www.smarthunter.com", "contact@smarthunter.com"))
+                .build();
     }
 
     @Override
